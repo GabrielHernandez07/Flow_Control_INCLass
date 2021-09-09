93f11524475160298c41c97bc2d8928ee4bdd157
ghern <ghern@DESKTOP-OCUVVH0>
2021-09-09 10:51:58 -0500

    FizzBuzz

---
+++ FlowChart_InCLASS/FlowChart_InCLASS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FlowChart_InCLASS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randValue = rand.Next(1, 101);
+
+            if (randValue % 3 == 0 && randValue % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (randValue % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (randValue % 3 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else
+
+            Console.WriteLine(randValue);
+        }
+    }
+}
